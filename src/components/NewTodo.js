150e4d239f0e0f5318bc65d51f08c5eee8088c2e
jeehee1 <jeehee1w@gmail.com>
2022-11-17 23:06:02 +0000

    add new Todo submit handler

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,9 +1,25 @@
+import { useContext, useRef } from "react";
+import { TodosContext } from "../context/todos-context";
+
 const NewTodo = () => {
+  const todosCtx = useContext(TodosContext);
+  const enteredTodoText = useRef();
+  const enteredTodoDate = useRef();
+
+  const submitTodoHandler = (event) => {
+    event.preventDefault();
+
+    const newTodoText = enteredTodoText.current.value;
+    const newTodoDate = enteredTodoDate.current.value;
+
+    todosCtx.addTodo(newTodoDate, newTodoText);
+  };
+
   return (
-    <form>
-      <label>New Todo</label>
-      <input type="date" />
-      <input type="text" />
+    <form onSubmit={submitTodoHandler}>
+      <label htmlFor="todoText">New Todo</label>
+      <input type="date" name="todoDate" ref={enteredTodoDate} />
+      <input type="text" id="todoText" name="todoText" ref={enteredTodoText} />
       <button>Add Todo</button>
     </form>
   );
