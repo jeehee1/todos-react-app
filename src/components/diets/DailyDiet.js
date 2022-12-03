cb975f23593829a985ac91ee96e91fe84d93e671
jeehee1 <jeehee1w@gmail.com>
2022-12-03 21:13:46 +0000

    delete unnecessary DietItem component

--- src/components/diets/DailyDiet.js
+++ src/components/diets/DailyDiet.js
@@ -1,5 +1,3 @@
-import DietItem from "./DietItem";
-
 const DailyDiet = (props) => {
   const mealType = ["Breakfast", "Lunch", "Dinner", "Snacks"];
   console.log("Its DailyDiet component");
@@ -9,25 +7,11 @@ const DailyDiet = (props) => {
   return (
     <li>
       {mealType.map((type) => (
-        <DietItem diet={props.diet} type={type}/>
+            <div>
+            <h4>{type}</h4>
+            <p>{props.diet[type] || " - "}</p>
+          </div>
       ))}
-      {/* <div>{props.diet.date}</div>
-      <div>
-        <h3>Breakfast</h3>
-        {props.diet.Breakfast}
-      </div>
-      <div>
-        <h3>Lunch</h3>
-        {props.diet.Lunch}
-      </div>
-      <div>
-        <h3>Dinner</h3>
-        {props.diet.Dinner}
-      </div>
-      <div>
-        <h3>Snacks</h3>
-        {props.diet.Snack}
-      </div> */}
     </li>
   );
 };
