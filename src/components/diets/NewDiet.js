0688fb800d1f8a5c443a9564233cecace4e0dfe6
jeehee1 <jeehee1w@gmail.com>
2022-12-14 09:07:51 +0000

    add type select input into new diet

--- src/components/diets/NewDiet.js
+++ src/components/diets/NewDiet.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import Select from "react-select";
+
+const options = [
+  { value: "breakfast", label: "Breakfast" },
+  { value: "lunch", label: "Lunch" },
+  { value: "dinner", label: "Dinner" },
+  { value: "snacks", label: "Snacks" },
+];
+
 const NewDiet = () => {
+  const [typeSelect, setTypeSelect] = useState(null);
   return (
     <div>
       <form>
         <label htmlFor="date">Date</label>
         <input type="date" id="date" />
-        <label htmlFor="type">Meal Type</label>
-        <select id="type">
-          <option value="breakfast">Breakfast</option>
-          <option value="lunch">Lunch</option>
-          <option value="dinner">Dinner</option>
-          <option value="snack">Snack</option>
-        </select>
+        <label htmlFor='meal-type'>Meal Type</label>
+        <Select
+          id='meal-type'
+          defaultValue={typeSelect}
+          onChange={setTypeSelect}
+          options={options}
+        />
         <label htmlFor="meal">Plan</label>
         <input type="text" id="meal" />
         <button>Add Plan</button>
