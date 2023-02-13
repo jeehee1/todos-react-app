e8deba4c945c82810bdf6f38c70e74a27ac51e09
jeehee1 <jeehee1w@gmail.com>
2023-02-13 20:25:39 +0000

    add submit handler for schedules update

--- src/components/schedule/UpdateSchedule.js
+++ src/components/schedule/UpdateSchedule.js
@@ -1,22 +1,59 @@
+import { useState, useRef } from "react";
+import useHttp from "../../hooks/http";
+import Select from "react-select";
+
 const UpdateSchedule = () => {
-  const timeOpt = [];
+  const { sendRequest, loading, error } = useHttp();
+  const timeOptions = [];
   for (let i = 6; i < 24; i++) {
-    timeOpt.push(<option value={i}>{i}</option>);
+    timeOptions.push({ value: i, label: i });
   }
+  const dateRef = useRef();
+  const scheduleRef = useRef();
+  const [selectedStartTime, setSelectedStartTime] = useState();
+  const [selectedEndTime, setSelectedEndTime] = useState();
+
+  const updateScheduleHandler = (event) => {
+    event.preventDefault();
+    const selectedDate = dateRef.current.value;
+    const scheduleDetail = scheduleRef.current.value;
+    sendRequest(
+      `https://todos-project-a5fb8-default-rtdb.firebaseio.com/schedules/${selectedDate}.json`,
+      "POST",
+      JSON.stringify({
+        start: selectedStartTime.value,
+        end: selectedEndTime.value,
+        schedule: scheduleDetail,
+      }),
+      null,
+      "UPDATE_SCHEDULE"
+    );
+  };
+
   return (
-    <form>
+    <form onSubmit={updateScheduleHandler}>
       <label id="date">Date</label>
-      <input id="date" type="date" />
+      <input id="date" type="date" ref={dateRef} />
       <label id="startTime">Start Time</label>
-      <select name="startTime" id="startTime">
-        {timeOpt}
-      </select>
+      <Select
+        id="startTime"
+        value={selectedStartTime}
+        onChange={(selectedValue) => {
+          setSelectedStartTime(selectedValue);
+        }}
+        options={timeOptions}
+      />
       <label id="endTime">End Time</label>
-      <select name="endTime" id="endTime">
-        {timeOpt}
-      </select>
+      <Select
+        id="endTime"
+        value={selectedEndTime}
+        onChange={(selectedValue) => {
+          setSelectedEndTime(selectedValue);
+        }}
+        options={timeOptions}
+      />
       <label id="schedule">Schedule</label>
-      <input id="schedule" type="text" />
+      <input id="schedule" type="text" ref={scheduleRef} />
       <button>Submit</button>
     </form>
   );
