f922307f3023554f31d3816fa5256fb98b40bbba
jeehee1 <jeehee1w@gmail.com>
2023-02-15 22:34:56 +0000

    show schedule in DailySchedule component

--- src/components/schedule/DailySchedule.js
+++ src/components/schedule/DailySchedule.js
@@ -8,62 +8,41 @@ const DailySchedule = (props) => {
   const example = schedules.map((data) => data.start > 3);
   console.log(example);
 
-  // for (let i = 0; i < 24; i++) {
-  //   if (schedules) {
-  //     for (const schedule of schedules) {
-  //       if (schedule.start === i) {
-  //         <tr>
-  //           <th>{`${i}:00`}</th>
-  //           <td rowSpan={schedule.end - schedule.start}>{schedule.schedule}</td>
-  //         </tr>;
-  //       } else {
-  //       <tr>
-  //         <th>{`${i}:00`}</th>
-  //         <td></td>
-  //       </tr>;
-  //     }
-  //   }
-  //   scheduleList.push(
-  //     <tr>
-  //       <th>{`${i}:00`}</th>
-  //       <td></td>
-  //     </tr>
-  //   );
-  // }
+  for (let i = 0; i < 24; i++) {
+    if (schedules) {
+      const matchedTime = schedules.filter((data) => data.start === i);
+      // if (matchedTime.length > 0) {
+      //   console.log(matchedTime[0]);
+      //   console.log(matchedTime[0].end - matchedTime[0].start);
+      // }
+      matchedTime.length > 0
+        ? scheduleList.push(
+            <tr>
+              <th>{`${i}:00`}</th>
+              <td rowSpan={matchedTime[0].end - matchedTime[0].start}>
+                {matchedTime[0].schedule}
+              </td>
+            </tr>
+          )
+        : scheduleList.push(
+            <tr>
+              <th>{`${i}:00`}</th>
+            </tr>
+          );
+    } else {
+      <tr>
+        <th>{`${i}:00`}</th>
+      </tr>;
+    }
+  }
+  console.log(scheduleList);
 
   return (
     <Fragment>
       <h4>2023-2-1 Monday</h4>
       <div className={classes.schedule}>
         <table>
-          {/* <tbody className={classes.schedule}>{scheduleList}</tbody> */}
-
-          <tbody>
-            <tr>
-              <th>1</th>
-              <td rowSpan="3">sdfaw</td>
-            </tr>
-            <tr>
-              <th>2</th>
-            </tr>
-            <tr>
-              <th>3</th>
-            </tr>
-            <tr>
-              <th>4</th>
-              <td>skdfjlaw</td>
-            </tr>
-            <tr>
-              <th>5</th>
-            </tr>
-            <tr>
-              <th>6</th>
-            </tr>
-            <tr>
-              <th>7</th>
-              <td>klajfekl</td>
-            </tr>
-          </tbody>
+          <tbody className={classes.schedule}>{scheduleList}</tbody>
         </table>
       </div>
     </Fragment>
